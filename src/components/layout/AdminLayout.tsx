d8fc6fadfbb30c0dfffcba1feff220213be1182e
T R Methu <fmethubd@gmail.com>
2024-01-20 11:01:55 +0600

    role authentication added on sidebar hardcoded

--- src/components/layout/AdminLayout.tsx
+++
@@ -1,9 +0,0 @@
-const AdminLayout = () => {
-    return (
-        <div>
-            <h1>This is AdminLayout components</h1>
-        </div>
-    );
-};
-
-export default AdminLayout;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,27 +3,28 @@ import App from "../App";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import { adminPaths } from "./admin.routes";
+import { routeGenerator } from "../utils/routesGenerator";
+import { facultyPaths } from "./faculty.routes";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
     },
-
     {
         path: "/admin",
         element: <App />,
-        children: adminPaths,
+        children: routeGenerator(adminPaths),
     },
     {
         path: "/faculty",
         element: <App />,
-        children: adminPaths,
+        children: routeGenerator(facultyPaths),
     },
     {
         path: "/student",
         element: <App />,
-        children: adminPaths,
+        children: routeGenerator(adminPaths),
     },
 
     {
